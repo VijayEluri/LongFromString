d2d675eeea2ce8c12affc6c88a36b3fa9ec9ae36
beepscore <support@beepscore.com>
2010-12-04 12:24:15 -0800

    Add method longFromString().  Currently method always returns 1234.

--- src/com/beepscore/LongFromString/LongFromString.java
+++ src/com/beepscore/LongFromString/LongFromString.java
@@ -10,15 +10,21 @@ public class LongFromString {
 	{
 		Scanner inScan = new Scanner(System.in);
 		String inputString;
-		long myLong;
 		
 		// Prompt user to type information
 		System.out.println("Please enter number, digits 0-9.");		
 		inputString = inScan.next();
 		System.out.println("You typed " + inputString);
 
-		System.out.println("Please enter number, digits 0-9.");		
-		myLong = inScan.nextLong();
-		System.out.println("myLong = " + myLong);
+		System.out.println("Long = " + longFromString(inputString));
+	}
+	
+    // Convert a string to a long
+	static long longFromString(String aString)
+	{
+		long myLong;
+		myLong = 1234;
+		
+		return myLong;
 	}
 }
