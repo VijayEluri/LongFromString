7fce1b868652e40e5990e796361c8bf8df065ba6
beepscore <support@beepscore.com>
2013-08-31 11:48:00 -0700

    Change tab to spaces.

--- src/com/beepscore/LongFromString/NumberTooBigException.java
+++ src/com/beepscore/LongFromString/NumberTooBigException.java
@@ -2,8 +2,8 @@ package com.beepscore.LongFromString;
 
 public class NumberTooBigException extends Exception 
 {
-	NumberTooBigException (String message)
-	{
-		super (message);
-	}
+    NumberTooBigException (String message)
+    {
+        super (message);
+    }
 }
